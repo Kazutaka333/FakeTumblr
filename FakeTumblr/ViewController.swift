bb0efe60b246fd4e77a3d44a2f99fa4dd4624eed
Kazutaka Homma <special18fender@gmail.com>
2019-02-15 19:25:46 -0800

    Initial Commit

---
+++ FakeTumblr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeTumblr
+//
+//  Created by Kazutaka Homma on 2/15/19.
+//  Copyright © 2019 Kazutaka Homma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
